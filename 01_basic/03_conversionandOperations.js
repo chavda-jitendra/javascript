c9baeb994fde0e36afa9bbe133c77b730ea40a2b
chavda-jitendra <161820769+chavda-jitendra@users.noreply.github.com>
2025-03-30 10:46:39 +0000

    conversionandOperations

---
+++ 01_basic/03_conversionandOperations.js
@@ -0,0 +1,22 @@
+// let score = "100"
+
+// console.log(typeof score)
+
+// let scorenumber = Number(score)
+
+// console.log(typeof scorenumber)
+// console.log(scorenumber);
+
+// "100abc" => NaN
+// true => 1; false => 3
+
+let scoreB = 200
+let scoreBBoolean = Boolean(scoreB)
+
+console.log(scoreBBoolean);
+
+let scorec = 200
+let scorecString = String(scorec)
+console.log(scorecString);
+console.log(typeof scorecString);
+
