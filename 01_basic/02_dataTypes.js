abc95f81f7ec609d8c063026d9a34271c052adf0
chavda-jitendra <161820769+chavda-jitendra@users.noreply.github.com>
2025-03-30 10:20:21 +0000

    datatypes

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict";//all code in new version
+let age = 20
+
+// alert(3+3) // we are using nodejs, not browser
+
+
+// number => 2 to power 53
+// string => ""
+// null => standalone value
+// bigint
+// undefined =>
+// boolean => true/false
+// symbol => unique
+
+
+// object
+
+console.log(typeof age)
+
