4acb0701830db4a91763fc0bdf22ed7819232b6f
chavda-jitendra <161820769+chavda-jitendra@users.noreply.github.com>
2025-03-30 18:33:53 +0000

    string

---
+++ 01_basic/04_string.js
@@ -0,0 +1,20 @@
+//define string in js
+const gameName = new String("Free Fire");
+// console.log(gameName);
+// console.log(gameName[0]);
+// console.log(gameName.length);
+console.log(gameName.charAt(3));
+console.log(gameName.indexOf('F'))
+
+// upper
+// lower
+// replace
+// split
+
+
+// const name = "jitendra"
+// const count = 50
+
+// console.log(name + count);
+
+// console.log(`hello my name is ${name}, and count is ${count}..`);
