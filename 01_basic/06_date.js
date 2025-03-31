9b913a87e301848654549aa0be8a0f25c5fb7a69
chavda-jitendra <161820769+chavda-jitendra@users.noreply.github.com>
2025-03-31 08:57:36 +0000

    date

---
+++ 01_basic/06_date.js
@@ -0,0 +1,15 @@
+
+let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(typeof myDate);  //object
+
+
+
+// create my dete
+// let createDate = new Date(2023, 0, 25)
+let createDate = new Date(2023,2,5,6,3)
+console.log(createDate.toDateString());
+console.log(createDate.toLocaleString());
+
+
