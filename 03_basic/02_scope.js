9dab9297f22b3a9ab0f5d7624ee24ffe1dbf159c
chavda-jitendra <161820769+chavda-jitendra@users.noreply.github.com>
2025-04-01 19:24:53 +0000

    scope

---
+++ 03_basic/02_scope.js
@@ -0,0 +1,19 @@
+// let a = 10
+// const b = 20
+// var c = 30
+
+
+
+function one(){
+    const username = "jitu"
+
+    function two(){
+        const useremail = "J@gmai.com"
+        console.log(username);
+    }
+    // console.log(useremail);
+
+    two()
+}
+
+one()
