4e5ce4d4e391bc74bf3028eded1b4abd09d51970
chavda-jitendra <161820769+chavda-jitendra@users.noreply.github.com>
2025-06-12 04:45:10 +0000

    arrow

--- 03_basic/03_Arrow.js
+++ 03_basic/03_Arrow.js
@@ -1,12 +1,12 @@
-const user = {
-    uname: "jitu",
-    price: 20,
+// const user = {
+//     uname: "jitu",
+//     price: 20,
 
-    welcome: function(){
-        console.log(`${this.uname}, welcome to website `);
+//     // welcome: function(){
+//     //     console.log(`${this.uname}, welcome to website `);
         
-    }
-}
+//     // }
+// }
 
 // user.welcome()
 // user.uname = "jitendra"
@@ -32,16 +32,17 @@ const user = {
 // }
 // code()
 
+// arrow function
 
+let two = (n1,n2) => {
+    return n1 + n2  // retun key word use
+}
+console.log(two(3,4));
 
-// const two = (num1,num2) =>{
-//     return num1+num2
-// }
-// console.log(two(20,10));
 
 
 // const two = (num1,num2) => num1+num2 // or if you use {} than return key word compulsory
 // const two = (num1,num2) => (num1+num2)
-const two = (num1,num2) => ({username:"jitu"}) //onject use in arrow function
+// const two = (num1,num2) => ({username:"jitu"}) //onject use in arrow function
 
-console.log(two(20,10));
+// console.log(two(20,10));
